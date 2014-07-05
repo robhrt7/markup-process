f7e36ee0487343017d2708b30d74e879f0e213de
Robert Haritonov <r@rhr.me>
2014-07-05 22:04:59 +0300

    Grunt optimization tasks: sprites, webp, CSS/HTML min

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,161 @@
+var path = require('path');
+
 module.exports = function(grunt) {
+    // Shows time spent on tasks
+    require('time-grunt')(grunt);
+
+    // Loading all grunt modules base on package.json
+    require('load-grunt-tasks')(grunt);
 
     // Tasks
     grunt.initConfig({
+        assets: 'build/assets',
+
+        clean: {
+            build: ["build"]
+        },
 
         less: {
             main: {
                 files: {
-                    "css/main.css": "css/less/main.less"
+                    "build/assets/css/main.css": "build/assets/css/main.less"
+                }
+            }
+        },
+
+        copy: {
+            // making working assets copy
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            'assets/**/*.*'
+                        ],
+                        dest: 'build/'
+                    }
+                ]
+            },
+
+            // copying sprited images bask to source — css-sprited.css -> css.css
+            sprited: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'build/assets/css',
+                        src: [
+                            '**/*-sprited.css'
+                        ],
+                        dest: 'build/assets/css/',
+                        rename: function(destBase, destPath){
+                            // renaming copied files
+                            return destBase + '/' + destPath.replace('-sprited.css', '.css');
+                        }
+                    }
+                ]
+            }
+        },
+
+        connect: {
+            main: {
+                options: {
+                    port: 8000,
+                    hostname: '*'
+                }
+            }
+        },
+
+        smartsprites: {
+            main: {
+                documentRootDirPath: './',
+                rootDirPath: 'build/assets/css',
+                outputDirPath: 'build/assets/css',
+                cssFileSuffix: '-sprited',
+                stderr: true
+            }
+        },
+
+        cwebp: {
+            main: {
+                options: {
+                    ext: '.webp'
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'build/assets',
+                        src: ['**/*.{png,jpg,gif}'],
+                        dest: 'build/assets'
+                    }
+                ]
+            }
+        },
+
+        htmlmin: {
+            main: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'build/index.html': 'index.html'
+                }
+            }
+        },
+
+        cssmin: {
+            main: {
+                expand: true,
+                cwd: 'build/assets/css',
+                src: [
+                    '**/*.css',
+                    '!**/*-sprited.css'
+                ],
+                dest: 'build/assets/css'
+            }
+        },
+
+        webpcss: {
+            main: {
+                options: {
+                    baseClass: '.webp',
+                    replace_from: /\.(png|jpg|jpeg)/,
+                    replace_to: '.webp'
+                },
+                files: {
+                    'build/assets/css/main.css': ['build/assets/css/main.css']
                 }
             }
         },
 
         watch: {
-            main: {
-                files: 'css/less/**/*.less',
-                tasks: ['less'],
+            css: {
+                files: 'assets/css/**/*.less',
+                tasks: ['newer:less'],
                 options: {
                     nospawn: true
                 }
             }
-        }
+        },
 
+        concurrent: {
+            target1: ['cssmin:main', 'cwebp:main','htmlmin:main']
+        }
     });
 
-    // Load plugins installed via npm install;
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-less');
+    // Dev tasks
+    grunt.registerTask('default', ['clean-build','copy:main', 'less:main']);
+    grunt.registerTask('watch-css', ['default', 'watch:css']);
 
-    // Default update tasl
-    grunt.registerTask('default', ['less']);
+    grunt.registerTask('serve', 'running a dev server', function(){
+        grunt.task.run('connect:main:keepalive');
+    });
+
+    // Production build
+    grunt.registerTask('build', ['default', 'sprite', 'cssmin:main', 'cwebp:main', 'htmlmin:main', 'webpcss:main']);
+    grunt.registerTask('build-conc', ['default', 'sprite', 'concurrent:target1', 'webpcss:main']);
+
+    // Misc
+    grunt.registerTask('sprite', ['smartsprites:main', 'copy:sprited']);
+    grunt.registerTask('clean-build', ['clean:build']);
 };
